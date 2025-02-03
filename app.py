e8146c0c6ed5b977a13bfc157a32d2eae013811a
unknown <Administrator@CIC-SME.CA>
2025-02-03 15:12:17 -0400

    Add Procfile and configure port

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is your Heroku app.'
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
